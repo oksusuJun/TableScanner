2f4627106c7cdeec1eeb344873689d15e23b07eb
JungSoochan <wjdtncks007@naver.com>
2017-11-27 09:31:01 +0900

    .

--- TableScanner/src/main/java/noshow/member/dao/MemberDAO.java
+++ TableScanner/src/main/java/noshow/member/dao/MemberDAO.java
@@ -1,5 +1,21 @@
 package noshow.member.dao;
 
+import org.apache.ibatis.session.SqlSession;
+
+import noshow.vo.Member;
+
 public interface MemberDAO {
+
+	//member DB저장
+	int insertMember(SqlSession session, Member member);
+	
+	//member DB수정
+	int updateMemberById(SqlSession session, Member member);
+	
+	//member DB삭제
+	int deleteMemberById(SqlSession session, String email);
+	
+	//member ID으로 조회
+	Member selectMemberById(SqlSession session, String email);
 	
 }
