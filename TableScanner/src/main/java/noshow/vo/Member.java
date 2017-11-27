ea64b6a4cc4c190c1421849523055be5d50539e4
YoonDongUng <qwe7662@naver.com>
2017-11-27 09:37:28 +0900

    member vo완성

--- TableScanner/src/main/java/noshow/vo/Member.java
+++ TableScanner/src/main/java/noshow/vo/Member.java
@@ -9,5 +9,159 @@ public class Member {
 	private String memberTel; /* 전화번호 */
 	private String memberBirthday; /* 생년월일 */
 	private String memberEmail; /* 이메일 */
+	
+	// 생성자
+	public Member() {}
+	
+	public Member(String memberId, String memberPassword, String memberName, int memberAge, String memberGender,
+			String memberTel, String memberBirthday, String memberEmail) {
+		this.memberId = memberId;
+		this.memberPassword = memberPassword;
+		this.memberName = memberName;
+		this.memberAge = memberAge;
+		this.memberGender = memberGender;
+		this.memberTel = memberTel;
+		this.memberBirthday = memberBirthday;
+		this.memberEmail = memberEmail;
+	}
 
+	//getters and setters
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+	public String getMemberPassword() {
+		return memberPassword;
+	}
+
+	public void setMemberPassword(String memberPassword) {
+		this.memberPassword = memberPassword;
+	}
+
+	public String getMemberName() {
+		return memberName;
+	}
+
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+
+	public int getMemberAge() {
+		return memberAge;
+	}
+
+	public void setMemberAge(int memberAge) {
+		this.memberAge = memberAge;
+	}
+
+	public String getMemberGender() {
+		return memberGender;
+	}
+
+	public void setMemberGender(String memberGender) {
+		this.memberGender = memberGender;
+	}
+
+	public String getMemberTel() {
+		return memberTel;
+	}
+
+	public void setMemberTel(String memberTel) {
+		this.memberTel = memberTel;
+	}
+
+	public String getMemberBirthday() {
+		return memberBirthday;
+	}
+
+	public void setMemberBirthday(String memberBirthday) {
+		this.memberBirthday = memberBirthday;
+	}
+
+	public String getMemberEmail() {
+		return memberEmail;
+	}
+
+	public void setMemberEmail(String memberEmail) {
+		this.memberEmail = memberEmail;
+	}
+	
+	// tostring
+	@Override
+	public String toString() {
+		return "Member [memberId=" + memberId + ", memberPassword=" + memberPassword + ", memberName=" + memberName
+				+ ", memberAge=" + memberAge + ", memberGender=" + memberGender + ", memberTel=" + memberTel
+				+ ", memberBirthday=" + memberBirthday + ", memberEmail=" + memberEmail + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + memberAge;
+		result = prime * result + ((memberBirthday == null) ? 0 : memberBirthday.hashCode());
+		result = prime * result + ((memberEmail == null) ? 0 : memberEmail.hashCode());
+		result = prime * result + ((memberGender == null) ? 0 : memberGender.hashCode());
+		result = prime * result + ((memberId == null) ? 0 : memberId.hashCode());
+		result = prime * result + ((memberName == null) ? 0 : memberName.hashCode());
+		result = prime * result + ((memberPassword == null) ? 0 : memberPassword.hashCode());
+		result = prime * result + ((memberTel == null) ? 0 : memberTel.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Member other = (Member) obj;
+		if (memberAge != other.memberAge)
+			return false;
+		if (memberBirthday == null) {
+			if (other.memberBirthday != null)
+				return false;
+		} else if (!memberBirthday.equals(other.memberBirthday))
+			return false;
+		if (memberEmail == null) {
+			if (other.memberEmail != null)
+				return false;
+		} else if (!memberEmail.equals(other.memberEmail))
+			return false;
+		if (memberGender == null) {
+			if (other.memberGender != null)
+				return false;
+		} else if (!memberGender.equals(other.memberGender))
+			return false;
+		if (memberId == null) {
+			if (other.memberId != null)
+				return false;
+		} else if (!memberId.equals(other.memberId))
+			return false;
+		if (memberName == null) {
+			if (other.memberName != null)
+				return false;
+		} else if (!memberName.equals(other.memberName))
+			return false;
+		if (memberPassword == null) {
+			if (other.memberPassword != null)
+				return false;
+		} else if (!memberPassword.equals(other.memberPassword))
+			return false;
+		if (memberTel == null) {
+			if (other.memberTel != null)
+				return false;
+		} else if (!memberTel.equals(other.memberTel))
+			return false;
+		return true;
+	}
+
+	
+	
 }
