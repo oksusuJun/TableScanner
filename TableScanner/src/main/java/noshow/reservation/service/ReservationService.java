d3f27a4dfe3a75134ec2a4a9c58269bd6ba5352b
HyeonJun <phj6207@naver.com>
2017-11-28 15:44:54 +0900

    예약

--- TableScanner/src/main/java/noshow/reservation/service/ReservationService.java
+++ TableScanner/src/main/java/noshow/reservation/service/ReservationService.java
@@ -1,5 +1,16 @@
 package noshow.reservation.service;
 
+import java.util.List;
+
+import noshow.vo.Reservation;
+
 public interface ReservationService {
 	
+	int addReservation(Reservation reservation);
+	
+	int updateReservation(Reservation reservation);
+	
+	int deleteReservation(int resNum);
+	
+	List<Reservation> selectReservationByMemberId(String memberId);
 }
